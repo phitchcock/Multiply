937ab116c2f3ce6c3c8e8179ad1cdf05db26df08
phitchcock <phitchcock@me.com>
2014-09-29 15:57:25 -0700

    initial commit

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -8,7 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    //IBOutlets
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var multiplierLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var multiplierSlider: UISlider!
+    @IBOutlet weak var operatorSegmentControl: UISegmentedControl!
+
+    //IBActions
+    @IBAction func onCalculateButtonPressed(sender: AnyObject) {
+
+        var getNumber:Int = (numberTextField.text as NSString).integerValue
+        var getMultiplier:Int? = multiplierLabel.text?.toInt()
+        var answer = getNumber * getMultiplier!
+        var divAnswer = getNumber / getMultiplier!
+
+
+        if answer >= 20 {
+
+            self.view.backgroundColor = UIColor.greenColor()
+
+        } else {
+
+            self.view.backgroundColor = UIColor.whiteColor()
+        }
+
+        if operatorSegmentControl.selectedSegmentIndex == 0 {
+
+            answerLabel.text = "\(answer)"
+
+        } else {
+
+            answerLabel.text = "\(divAnswer)"
+        }
+
+
+        numberTextField.endEditing(true)
+
+        //numberTextField.text = nil
+
+    }
+
+    @IBAction func sliderAction(sender: AnyObject) {
+
+        var fixedValue = (multiplierSlider.value as NSNumber).integerValue
+
+        multiplierLabel.text = "\(fixedValue)"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +66,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+
+        self.view.endEditing(true)
+
     }
 
 
